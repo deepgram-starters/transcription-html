45b6fcf694a8cb1623d00deaa74f5850c14f053d
Luke Oliff <luke@lukeoliff.com>
2026-01-28 11:03:29 +0000

    fix: change Vite port from 8081 to 5173

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: '.',
   server: {
-    port: 8081,
+    port: 5173,
     open: false,
     host: true
   },
   preview: {
-    port: 8081,
+    port: 5173,
     open: true,
     host: true
   },
